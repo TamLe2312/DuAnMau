38bb3fa2162775ec5866a2fc36ccc15d18c4d9da
Michael <tamle23122004@gmail.com>
2023-09-15 23:19:53 +0700

    Set up environment

---
+++ src/controllers/homeController.js
@@ -0,0 +1,25 @@
+const connection = require('../config/database');
+const { use } = require('../routes/web');
+
+const getHomepage = (req, res) => {
+
+    let users = [];
+
+    connection.query(
+        'SELECT * FROM Users',
+        function (err, results, fields) {
+            users = results
+            console.log('result homepage = ', results);
+
+            console.log("Users : ", users);
+            res.send(JSON.stringify(users));
+        }
+    );
+
+}
+const getTest = (req, res) => {
+    res.render('sample.ejs')
+}
+module.exports = {
+    getHomepage, getTest
+}
