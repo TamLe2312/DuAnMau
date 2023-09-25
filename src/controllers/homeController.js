eef089d3730fde46b6f4b4060ddafaacfe67c240
Michael <tamle23122004@gmail.com>
2023-09-25 23:20:52 +0700

    Resolve conflict

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,25 +1,14 @@
-const connection = require('../config/database');
-const { use } = require('../routes/web');
+const connection = require("../config/database");
+// const { use } = require("../routes/web");
 
 const getHomepage = (req, res) => {
-
-    let users = [];
-
-    connection.query(
-        'SELECT * FROM Users',
-        function (err, results, fields) {
-            users = results
-            console.log('result homepage = ', results);
-
-            console.log("User Information : ", users);
-            res.send(JSON.stringify(users));
-        }
-    );
-
-}
+    res.send("hello world from FplHub");
+};
 const getTest = (req, res) => {
-    res.render('sample.ejs')
-}
+    res.render("sample.ejs");
+};
+
 module.exports = {
-    getHomepage, getTest
-}
+    getHomepage,
+    getTest,
+};
