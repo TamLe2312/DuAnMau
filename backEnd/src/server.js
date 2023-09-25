f29aa7ac30ecc7f86879e05ceec8f90869d4688a
ccxtncntus <ccxtncn00@gmail.com>
2023-09-25 23:16:14 +0700

    update newlayout

---
+++ backEnd/src/server.js
@@ -0,0 +1,28 @@
+// require("dotenv").config();
+const express = require("express");
+var bodyParser = require("body-parser");
+const path = require("path");
+const configViewEngine = require("./config/viewEngine");
+const webRoutes = require("./routes/web");
+const api = require("./routes/api");
+const connection = require("./config/database");
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
+
+configViewEngine(app);
+
+app.use("/", webRoutes);
+
+// api account
+app.use("/account", api);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- src/routes/web.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getHomepage } = require('../controllers/homeController');
-const { getTest } = require('../controllers/homeController');
-const Router = express.Router();
-
-Router.get('/', getHomepage)
-Router.get('/test', getTest)
-
-module.exports = Router;
